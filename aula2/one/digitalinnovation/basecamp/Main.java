49fc48c72b8a87caff7a0585be3894bd9adfc4e9
carlinhoshk <carlosmdohk@gmail.com>
2022-04-21 17:44:36 -0300

    Subindo aula 2 de variveis, tipo de dados e operadores math

---
+++ aula2/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,35 @@
+package one.digitalinnovation.basecamp;
+
+/*
+ 21/04/2022
+ 17:17
+
+ Exercicio aula 2 de variaveis, tipo de dados e operadores aritméticos.
+
+
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* Todos os numeros dentro */
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 100000000000000000L;
+        long l2 = 200040000000550000L;
+        /* são inteiros, o que muda é quantidade de que cabe em cada um */
+
+
+
+
+    }
+
+
+}
